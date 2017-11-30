8198c5e4b6b05bb6753954078fdaf160ba1896b7
YUXIN LI <31746106+liyuxin2017@users.noreply.github.com>
2017-11-29 18:29:56 -0800

    Update Runner.java

--- src/com.neuSep17/main/Runner.java
+++ src/com.neuSep17/main/Runner.java
@@ -4,7 +4,6 @@ public class Runner {
 
 	public static void main(String[] args) {
 		InventoryManagementFrame i = new InventoryManagementFrame();
-		i.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
