d670d491112382c506643528474dec21f290379c
YUXIN LI <31746106+liyuxin2017@users.noreply.github.com>
2017-11-29 17:31:00 -0800

    main

---
+++ src/com.neuSep17/main/Runner.java
@@ -0,0 +1,12 @@
+package project;
+
+import javax.swing.JFrame;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		InventoryManagementFrame i = new InventoryManagementFrame();
+		i.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
