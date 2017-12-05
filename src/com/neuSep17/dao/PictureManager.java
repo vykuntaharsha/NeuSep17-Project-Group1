ecdcdbbd492255ccb44ab04a243952cff4a88047
Bin Shi <i@shibin.info>
2017-12-05 10:45:46 -0800

    refactory and fixing some bugs

--- src/com/neuSep17/dao/PictureManager.java
+++ src/com/neuSep17/dao/PictureManager.java
@@ -5,6 +5,7 @@ import java.awt.image.*;
 import java.io.*;
 import java.net.*;
 import javax.imageio.*;
+import javax.swing.SwingUtilities;
 
 import com.neuSep17.dto.Vehicle;
 
@@ -19,94 +20,99 @@ import com.neuSep17.dto.Vehicle;
  * 
  */
 public class PictureManager {
-
     //root direction of the picture files
     private static final String PICTURE_DIR = "picture";
+    private static final boolean DEBUG = false;
 
     /**
-     * Get the image of a vehicle using the photoURL property.
-     * @param vehicle
+     * Get the image of a vehicle using its photoURL.
+     * @param v
      * @return The image of the vehicle or null if there is no valid image.
      */
-    public static Image getVehicleImage(Vehicle vehicle) {
-        return getVehicleImage(vehicle.getPhotoURL());
+    public static Image getVehiclePhoto(Vehicle v) {
+        if(v==null || v.getPhotoURL() == null || v.getPhotoURL().getFile().isEmpty() ) return null;
+        else return getVehicleImage(v.getPhotoURL());
     }
 
-    public static BufferedImage getVehicleImage(URL photoURL) {
-        File file = new File(PICTURE_DIR, getFileFullName(photoURL.toString()));
-        if (file.exists()) {
-            return loadImageFromDisk(photoURL.toString());
-        } else {
-            BufferedImage image = laodImageFromURL(photoURL);
-            cacheImage(image, photoURL.toString());
-            return image;
-        }
-    }
-
-    public static String getFileHashName(String imageURL) {
-        return String.valueOf(imageURL.hashCode());
-    }
-
-    private static String getFileExt(String imageURL) {
-        int dot = imageURL.lastIndexOf('.');
-        String fileExt = imageURL.substring(dot+1);
-        return fileExt;
-    }
-
-    /**
-     * Get the full file name (hashed) from the image URL.
-     * @param imageURL
-     * @return the full file name in the format of hashcode.ext
-     */
-    private static String getFileFullName(String imageURL) {
-        return getFileHashName(imageURL) +"."+ getFileExt(imageURL);
-    }
-
-    public static BufferedImage loadImageFromDisk(String imageURL) {
+    private static BufferedImage getVehicleImage(URL photoURL) {
         BufferedImage image = null;
-        try {
-            File file = new File(PICTURE_DIR, getFileFullName(imageURL));
-            image = ImageIO.read(file);
-        } catch (IOException e) {
-
+        File file = new File(PICTURE_DIR, getFullName(photoURL.getFile()));
+        if (file.exists()) {
+            if (file.length() > 0)
+                image = loadImageFromDisk(file);
+        } else {
+            image = loadImageFromURL(photoURL);
+            cacheImage(image, getFullName(photoURL.getFile()));
         }
         return image;
     }
 
-    public static BufferedImage laodImageFromURL(URL url) {
-        BufferedImage image = null;
+    /**
+     * Get the full file name (hashed) from the URL's file like /5/6/7/13918653765x90.jpg
+     * @param urlFile
+     * @return the full file name in the format of hashcode.ext
+     */
+    private static String getFullName(String urlFile) {
+        return getFileName(urlFile) + "." + getFileExt(urlFile);
+    }
+    
+    private static String getFileName(String urlFile) {
+        return String.valueOf(urlFile.hashCode());
+    }
 
+    private static String getFileExt(String urlFile) {
+        int dot = urlFile.lastIndexOf('.');
+        String ext = urlFile.substring(dot+1);
+        return ext;
+    }
+
+    public static BufferedImage loadImageFromDisk(File imageFile) {
+        BufferedImage image = null;
         try {
-            image = ImageIO.read(url);
+            image = ImageIO.read(imageFile);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Cannot read from disk.");
+            if(DEBUG) e.printStackTrace();
         }
+//        System.out.println("loaded from disk " + image.toString());
+        return image;
+    }
+
+    public static BufferedImage loadImageFromURL(URL url) {
+        BufferedImage image = null;
+        try {
+            if(url.openStream()!=null) image = ImageIO.read(url);
+        } catch (IOException e) {
+            System.out.println("Cannot load the photo from " + url.toString());
+            if(DEBUG) e.printStackTrace();
+        }
+//        System.out.println("loaded from URL " + image.toString());
         return image;
     }
     
-    protected static void cacheImage(BufferedImage image, String imageURL){
-        cacheImage(image, getFileFullName(imageURL), getFileExt(imageURL));
-    }
-
-    private static void cacheImage(BufferedImage image, String fileFullName, String fileExt) {
-        try { 
-            File imageFile = new File(PICTURE_DIR, fileFullName);
-            if(!imageFile.exists()) imageFile.createNewFile();
-            
-            ImageIO.write(image, fileExt, imageFile);
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static void cacheImage(BufferedImage image, String fileName) {
+        File file = new File(PICTURE_DIR, fileName);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+                ImageIO.write(image, getFileExt(fileName), file);
+            } catch (IOException e) {
+                System.out.println("Cannot cache the photo.");
+                if(DEBUG) e.printStackTrace();
+            }
         }
     }
+        
     
     public static void main(String[] args){
         URL url=null;
         try {
-            url = new URL("http://inventory-dmg.assets-cdk.com/RTT/Chevrolet/2016/2875373/default/ext_GBA_deg01x90.jpg");
+            url = new URL("http://inventory-dmg.assets-cdk.com/chrome_jpgs/2016/15879x90.jpg");
+//            url = new URL("http://inventory-dmg.assets-cdk.com/5/6/7/13918653765x90.jpg");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        System.out.println(PictureManager.getVehicleImage(url));
+        PictureManager.getVehicleImage(url);
     }
     
 }
