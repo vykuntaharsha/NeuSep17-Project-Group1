b1864824dd04f85449bd070bece9aa0dd4968e75
Sai Diksha <31628457+diksha2992@users.noreply.github.com>
2017-12-06 12:47:23 -0800

    Update ManageIncentivesScreen.java

--- src/com/neuSep17/ui/ManageIncentivesScreen.java
+++ src/com/neuSep17/ui/ManageIncentivesScreen.java
@@ -1,60 +1,50 @@
 package com.neuSep17.ui;
 
+import com.neuSep17.dao.IncentiveImple;
+import com.neuSep17.dto.Incentive;
+
+import java.awt.EventQueue;
+
 import javax.swing.*;
+
 import java.awt.*;
-
-public class ManageIncentivesScreen extends JFrame{
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+public class ManageIncentivesScreen extends UI
+{
     private static JFrame mainFrame;
     private JPanel headerPanel;
-    private JPanel filterPanel;
-    private JPanel toolPanel;
+    private JPanel footerPanel;
     private JPanel listPanel;
-
-    // GridBadConstraints shortcut methods
-    public class GBC extends GridBagConstraints {
-
-        public GBC(int gridx, int gridy) {
-            this.gridx = gridx;
-            this.gridy = gridy;
-        }
-
-        public GBC(int gridx, int gridy, int gridwidth, int gridheight) {
-            this.gridx = gridx;
-            this.gridy = gridy;
-            this.gridwidth = gridwidth;
-            this.gridheight = gridheight;
-        }
-
-        public GBC setAnchor(int anchor) {
-            this.anchor = anchor;
-            return this;
-        }
-
-        public GBC setFill(int fill) {
-            this.fill = fill;
-            return this;
-        }
-
-        public GBC setWeight(double weightx, double weighty) {
-            this.weightx = weightx;
-            this.weighty = weighty;
-            return this;
-        }
-
-        public GBC setInsets(int top, int left, int bottom, int right) {
-            this.insets = new Insets(top, left, bottom, right);
-            return this;
-        }
-
-        public GBC setIpad(int ipadx, int ipady) {
-            this.ipadx = ipadx;
-            this.ipady = ipady;
-            return this;
-        }
-    }
-
-    public IncentiveScreen() {
+    private JPanel sortPanel;
+    private JButton addButton;
+    private JLabel[] label;
+    private JButton[] editButtons;
+    private JButton[] deleteButtons;
+    private JPanel[] jp;
+    private ImageIcon[] ii;
+    private boolean asc=false;
+    private String sortoption="";
+    private String searchcontent="";
+    private JLabel[] imageLabel;
+    private JButton searchButton;
+    private int length;
+    private ArrayList<Incentive> incentives;//to save incentives read from file
+    private ArrayList<Incentive> Allincentives;
+    private String dealerid;
+    private JComboBox jsort;
+    private JButton searchby;
+    private JLabel sort;
+    private JComboBox order;
+    private JTextField jtf;
+    private JButton viewAll;
+    //  private IncentiveImple;
+    public ManageIncentivesScreen() throws FileNotFoundException
+    {
+        this.dealerid="gmps-bresee"; //TODO: Update this
         mainFrame = new JFrame();
         initialize();
         createComponents();
@@ -63,91 +53,244 @@ public class ManageIncentivesScreen extends JFrame{
         makeThisVisible();
     }
 
-    private void initialize() {
+    private void initialize() throws FileNotFoundException {
+        IncentiveImple iImple=new IncentiveImple();
+        incentives=iImple.getIncentivesForDealer(dealerid);
+        Allincentives=incentives;
+        length=incentives.size();
+        label=new JLabel[length];
+        editButtons=new JButton[length];
+        deleteButtons=new JButton[length];
+        jp=new JPanel[length];
+        ii=new ImageIcon[length];
+        imageLabel=new JLabel[length];
         mainFrame.setTitle("Incentive Management");
         mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        mainFrame.setLayout(new GridBagLayout());
-        mainFrame.setSize(1280, 700);
+        mainFrame.setBackground(Color.lightGray);
+        mainFrame.setLayout(new BorderLayout());
     }
 
     public void createComponents() {
         headerPanel = new JPanel();
-        filterPanel = new JPanel();
-        toolPanel = new JPanel();
+        headerPanel.setLayout(new GridLayout(2,1,10,10));
+
+        footerPanel = new JPanel();
+
         listPanel = new JPanel();
+        listPanel.setLayout(new GridLayout(length,1,10,10));
     }
 
     public void addComponents() {
         // Header
-        JLabel headerTitle = new JLabel("Incentive Management");
-        JButton inventoryButton = new JButton("Inventory");
-        JButton incentiveButton = new JButton("Incentive");
-        headerPanel.add(headerTitle);
-        headerPanel.add(inventoryButton);
-        headerPanel.add(incentiveButton);
-        headerPanel.setBackground(Color.lightGray);
-        headerPanel.setPreferredSize(new Dimension(1280, 100));
-        mainFrame.add(headerPanel, new GBC(0, 0, 2, 1).setFill(GBC.BOTH));
+        JLabel headerTitle = new JLabel("Incentive Lists and Management");
+        JPanel title=new JPanel();
+        title.add(headerTitle);
+        sortPanel=new JPanel();
+        sort=new JLabel("Sort by");
+        jsort=createComboBox(new String[]{"ID","Title","StartDate","EndDate","Discount"});
+        jtf=new JTextField(5);
+        searchButton=new JButton("search");
+        viewAll=new JButton("viewAll");
+        order=createComboBox(new String[]{"asc","desc"});
+        JLabel searchby=new JLabel("Search by ID");
+        sortPanel.add(sort);
+        sortPanel.add(jsort);
+        sortPanel.add(order);
+        sortPanel.add(searchby);
+        sortPanel.add(jtf);
+        sortPanel.add(searchButton);
+        sortPanel.add(viewAll);
+        headerPanel.add(title);
+        headerPanel.add(sortPanel);
+        mainFrame.add(headerPanel,BorderLayout.NORTH);
 
-        // Filter
-        JLabel filterTitle = new JLabel("Filter");
-        filterPanel.add(filterTitle);
-        filterPanel.setBackground(Color.white);
-        mainFrame.add(filterPanel, new GBC(0, 1, 1, 2).setAnchor(GBC.WEST).setIpad(200, 500).setFill(GBC.BOTH));
 
-        // Tool
-        JButton addButton = new JButton("Add");
-        addButton.setPreferredSize(new Dimension(100,20));
-        JLabel searchBar = new JLabel("SEARCH");
-        toolPanel.add(addButton);
-        toolPanel.add(searchBar);
-        toolPanel.setBackground(Color.lightGray);
-        mainFrame.add(toolPanel, new GBC(1,1).setAnchor(GBC.WEST).setFill(GBC.BOTH));
+        // Add button
+        addButton = createButton("Add New Incentive");
+//        addButton.setSize(200, 40);
+        footerPanel.add(addButton);
+        mainFrame.add(footerPanel, BorderLayout.SOUTH);
 
-        // List
-        listPanel.setBackground(Color.lightGray);
-        listPanel.setLayout(new GridLayout(5, 3, 5, 5));
-        createAndAddIncentiveRecord("description1");
-        createAndAddIncentiveRecord("description2");
-        createAndAddIncentiveRecord("description3");
-        createAndAddIncentiveRecord("description4");
-        createAndAddIncentiveRecord("description5");
-        mainFrame.add(listPanel, new GBC(1, 2).setFill(GBC.BOTH));
+        //add listpanel,according to sort and search
+        displayIncentives(incentives);
 
+        mainFrame.add(listPanel, BorderLayout.CENTER);
     }
 
-    /*
-    // Create incentive records table
-    public void createAndAddTable() {
-        // Create column names
-        String[] columnNames = {"Description"};
-
-        // Test data
-        Object[][] data = {{"Des1"}, {"Des2"}, {"Des3"}, {"Des4"}, {"Des5"}};
-
-        JTable table = new JTable(data, columnNames);
-        table.setBackground(Color.lightGray);
-
-        JScrollPane scrollPane = new JScrollPane(table);
+    private void displayIncentives(ArrayList<Incentive> incentives)
+    {
+        length=incentives.size();
+        for(int i=0;i<length;i++)
+        {
+            jp[i]=new JPanel();
+            ii[i]=new ImageIcon("hot.png");
+            imageLabel[i]=new JLabel(ii[i]);
+            imageLabel[i].setBounds(0, 0,24,31);
+            jp[i].add(imageLabel[i],new Integer(Integer.MIN_VALUE));
+            String content="ID"+" "+incentives.get(i).getID()+", "+incentives.get(i).getDescription();
+            // label[i]=new JLabel("Incentive ID"+" "+incentives.get(i).getID()+", "+incentives.get(i).getDescription());
+            if(content.length()>30)
+                content=content.substring(0,23)+"...";
+            label[i]=new JLabel(content);
+            label[i].setForeground(Color.blue);
+            editButtons[i]=new JButton("Edit");
+            deleteButtons[i]=new JButton("Delete");
+            jp[i].add(label[i]);
+            jp[i].add(editButtons[i]);
+            jp[i].add(deleteButtons[i]);
+            listPanel.add(jp[i]);
+        }
     }
-    */
 
-    // This method will be replaced by receiving an Incentive class
-    public void createAndAddIncentiveRecord(String description) {
-        JLabel desp = new JLabel(description);
-        JButton editButton = new JButton("Edit");
-        JButton deleteButton = new JButton("Delete");
-        listPanel.add(desp);
-        listPanel.add(editButton);
-        listPanel.add(deleteButton);
-    }
 
     public void addListeners() {
+        //    final IncentiveService is=new IncentiveService();
+	    	   /* jsort.addItemListener(new ItemListener()
+	    	    {
+
+					@Override
+					public void itemStateChanged(ItemEvent e) {
+						if(e.getStateChange()==ItemEvent.SELECTED)
+						{
+						//	IncentiveService is=new IncentiveService();
+							sortoption=(String)jsort.getSelectedItem();
+						//	incentives=is.getSortedIncentives(incentives, sortoption, asc);
+							displayIncentives(incentives);
+						}
+					}	
+	    	    });
+	    	    order.addItemListener(new ItemListener()
+	    	    {
+
+					@Override
+					public void itemStateChanged(ItemEvent e) {
+						if(e.getStateChange()==ItemEvent.SELECTED)
+						{
+							if(order.getSelectedItem().toString().equals("asc"))
+								asc=true;
+							else
+								asc=false;
+						//	incentives=is.getSortedIncentives(incentives, sortoption, asc);
+							displayIncentives(incentives);
+						}
+					}	
+	    	    });*/
+
+        searchButton.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e)
+            {
+                searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                searchButton.setForeground(Color.GRAY);
+            }
+            public void mouseExited(MouseEvent e) {
+                searchButton.setCursor(Cursor.getDefaultCursor());
+                searchButton.setForeground(Color.BLACK);
+            }
+            //@Override
+		 			/*public void mouseClicked(MouseEvent arg0) {
+		 				try
+		 				{
+		 				searchcontent=jtf.getText();//check if it's empty.
+		 				incentives=is.
+		 				
+		 				}
+		 				catch (Exception ex) {
+		 		            JOptionPane.showMessageDialog(this, ex.getMessage(), "Error: cannot be empty!", JOptionPane.ERROR_MESSAGE);
+		 		            ex.printStackTrace();
+		 		        }
+		 			}*/
+        });
+
+        for(int i=0;i<length;i++)
+        {
+            final JButton editButton=editButtons[i];
+            editButton.addMouseListener(new MouseAdapter() {
+                public void mouseEntered(MouseEvent e)
+                {
+                    editButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                    editButton.setForeground(Color.GRAY);
+                }
+                public void mouseExited(MouseEvent e) {
+                    editButton.setCursor(Cursor.getDefaultCursor());
+                    editButton.setForeground(Color.BLACK);
+                }
+                @Override
+                public void mouseClicked(MouseEvent arg0) {
+
+                }
+            });
+            final JButton deleteButton=deleteButtons[i];
+            deleteButton.addMouseListener(new MouseAdapter() {
+                public void mouseEntered(MouseEvent e)
+                {
+                    deleteButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                    deleteButton.setForeground(Color.GRAY);
+                }
+                public void mouseExited(MouseEvent e) {
+                    deleteButton.setCursor(Cursor.getDefaultCursor());
+                    deleteButton.setForeground(Color.BLACK);
+                }
+                @Override
+                public void mouseClicked(MouseEvent arg0)
+                {
+                    //return incentive i;
+                    mainFrame.dispose();
+                    try {
+                        ManageIncentivesScreen screen = new ManageIncentivesScreen();
+                    } catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            addButton.addMouseListener(new MouseAdapter() {
+                public void mouseEntered(MouseEvent e)
+                {
+                    addButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                    addButton.setForeground(Color.GRAY);
+                }
+                public void mouseExited(MouseEvent e) {
+                    addButton.setCursor(Cursor.getDefaultCursor());
+                    addButton.setForeground(Color.BLACK);
+                }
+                @Override
+                public void mouseClicked(MouseEvent arg0) {
+//                    AddIncentiveScreen a=new AddIncentiveScreen();
+                    try {
+//                        Incentive i=a.GetIncentive();
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            viewAll.addMouseListener(new MouseAdapter() {
+                public void mouseEntered(MouseEvent e)
+                {
+                    viewAll.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                    viewAll.setForeground(Color.GRAY);
+                }
+                public void mouseExited(MouseEvent e) {
+                    viewAll.setCursor(Cursor.getDefaultCursor());
+                    viewAll.setForeground(Color.BLACK);
+                }
+                @Override
+                public void mouseClicked(MouseEvent arg0) {
+                    displayIncentives(Allincentives);
+                }
+            });
+        }
 
     }
 
     private void makeThisVisible()
     {
+        mainFrame.setSize(800, 1000);//make it smaller and in the middle of the screen
+        UI ui=new UI();
+        ui.displayCenter(mainFrame);
+        mainFrame.setBackground(Color.white);
         mainFrame.setVisible(true);
     }
 
@@ -155,7 +298,7 @@ public class ManageIncentivesScreen extends JFrame{
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    IncentiveScreen screen = new IncentiveScreen();
+                    ManageIncentivesScreen screen = new ManageIncentivesScreen();
                 }
                 catch(Exception e) {
                     e.printStackTrace();
