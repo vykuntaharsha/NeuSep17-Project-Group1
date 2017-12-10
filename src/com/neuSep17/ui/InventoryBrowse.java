1ba2b36d5a94f7f18cb4f3aae37a04a4cb74e307
ManjulaPrabhu <31738763+ManjulaPrabhu@users.noreply.github.com>
2017-12-10 09:43:43 -0800

    Added the updated version

---
+++ src/com/neuSep17/ui/InventoryBrowse.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class BrowseInventory {
+    public static void main(String[] args) throws IOException {
+        BrowseInventoryScreen inventoryScreen =new BrowseInventoryScreen();
+        inventoryScreen.addListeners();
+
+    }
+}
