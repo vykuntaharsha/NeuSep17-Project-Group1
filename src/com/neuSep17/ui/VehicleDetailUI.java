c33eb7ca087c50ea51a392b0d0cff30928cf12a0
Bin Shi <i@shibin.info>
2017-12-13 09:39:26 -0800

    fix minor compile error in VehicleDetailsUI.java

--- src/com/neuSep17/ui/VehicleDetailUI.java
+++ src/com/neuSep17/ui/VehicleDetailUI.java
@@ -392,7 +392,7 @@ class VehicleDetailUI extends JFrame {
             {
                   FeatureCounter++;
             }
-		    else if(vehicle.getExteriorColor().toLowerCase().contains(value)||vehicle.getInteriorColor().toLowerCase().contains(value)))
+		    else if(vehicle.getExteriorColor().toLowerCase().contains(value)||vehicle.getInteriorColor().toLowerCase().contains(value))
             {
                   FeatureCounter++;
             }
